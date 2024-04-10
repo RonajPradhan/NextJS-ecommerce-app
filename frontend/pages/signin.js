e16830426ea11e3b074698c425eca55a8055d43e
Ronaj Pradhan <78714906+FunkyBuddha741@users.noreply.github.com>
2024-04-10 16:14:48 -0400

    Created two pages for signIn and reset

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,9 +1,22 @@
+import RequestReset from "../components/RequestReset";
 import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+import styled from "styled-components";
+
+const GridStyles = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+	grid-gap: 2rem;
+`;
 
 export default function SiginInPage() {
 	return (
-		<div>
-			<SignIn />
-		</div>
+		<>
+			<GridStyles>
+				<SignIn />
+				<SignUp />
+			</GridStyles>
+			<RequestReset />
+		</>
 	);
 }
